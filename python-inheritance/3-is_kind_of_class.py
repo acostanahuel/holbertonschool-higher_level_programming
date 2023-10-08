52de5fa3af4a8cbfac2fb5707fe1e4596ea0d005
acostanahuel <acostanahuel@gmail.com>
2023-10-08 13:49:58 -0700

    updates

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def is_kind_of_class(obj, a_class):
+    """Check if an object is an instance or inherited instance of a class."""
+    
+    if isinstance(obj, a_class):
+        return True
+    return False
