3ce54f42385b114a96ca5da6d5ac1b7f5403febf
acostanahuel <acostanahuel@gmail.com>
2023-10-08 14:55:04 -0700

    fix

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,5 +14,5 @@ class BaseGeometry:
         """Validate a parameter as an integer."""
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:i
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
