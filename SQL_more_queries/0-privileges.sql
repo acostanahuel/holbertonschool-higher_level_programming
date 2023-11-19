a9c269f0cffaa9e7bdec1508521ec06f0fab5888
acostanahuel <acostanahuel@gmail.com>
2023-11-19 20:17:57 -0300

    fix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Print all privileges of USER
 
-SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
